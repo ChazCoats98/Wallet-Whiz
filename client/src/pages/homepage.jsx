7180cef341274be91751f43629028d9fff0eaeac
Chaz Coats <chazcoats2@hotmail.com>
2024-01-11 09:54:39 -0500

    Laying out attention grabber page

--- client/src/pages/homepage.jsx
+++ client/src/pages/homepage.jsx
@@ -8,8 +8,25 @@ import laptop from '../assets/laptop.png'
 function homepage() {
     return (
         <div className="pageBox">
-            <div className="container pageInner">
-                <div className="container pageTextBox">
+            <div className="pageInner">
+                <div className="framer">
+                <div className="pageTextBox">
+                    <h2 className="headerText blackText">Why use WalletWhiz?</h2>
+                    <p className="descriptionSubText blackText">
+                        Managing your money can be hard and time-consuming<br />
+                        Your time is precious to you, why waste it on hours of book-keeping and stressful budgeting<br />
+                        Let WalletWhiz do the heavy lifting for you. WalletWhiz makes it quick and easy to keep an eye on
+                        your finances, so you can spend more time on what matters
+                    </p>
+                    <Link to='/register'>
+                    <Button variant='contained' disableElevation type='submit'>Register</Button>
+                    </Link>
+                </div>
+                    <img className='laptop' src={laptop}></img>
+            </div>
+                </div>
+            <div className='about1'>
+                <div className='pageTextBox'>
                     <h2 className="headerText">Why use WalletWhiz?</h2>
                     <p className="descriptionSubText">
                         Managing your money can be hard and time-consuming<br />
@@ -18,7 +35,6 @@ function homepage() {
                         your finances, so you can spend more time on what matters
                     </p>
                 </div>
-                    <img src={laptop}></img>
             </div>
         </div>
     );
