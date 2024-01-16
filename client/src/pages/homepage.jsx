6f282e5c61eecf5110e01a3014ed8762a84cb39d
Chaz Coats <chazcoats2@hotmail.com>
2024-01-16 08:05:09 -0500

    homepage changes

--- client/src/pages/homepage.jsx
+++ client/src/pages/homepage.jsx
@@ -13,10 +13,10 @@ function homepage() {
                 <div className="pageTextBox">
                     <h2 className="headerText blackText">Why use WalletWhiz?</h2>
                     <p className="descriptionSubText blackText">
-                        Managing your money can be hard and time-consuming<br />
+                    Managing your money can be hard, WalletWhiz makes it easy.<br />
                         Your time is precious to you, why waste it on hours of book-keeping and stressful budgeting<br />
-                        Let WalletWhiz do the heavy lifting for you. WalletWhiz makes it quick and easy to keep an eye on
-                        your finances, so you can spend more time on what matters
+                        WalletWhiz gives you the tools to manage your money with ease <br />
+                        so you can spend time on what really matters. 
                     </p>
                     <Link to='/register'>
                     <Button variant='contained' disableElevation type='submit'>Register</Button>
@@ -26,18 +26,41 @@ function homepage() {
             </div>
                 </div>
             <div className='about1'>
+                <div className='framer'>
                 <div className='pageTextBox'>
-                    <h2 className="headerText">Why use WalletWhiz?</h2>
+                    <h2 className="headerText">Stay on top of your spending.</h2>
                     <p className="descriptionSubText">
-                        Managing your money can be hard and time-consuming<br />
-                        Your time is precious to you, why waste it on hours of book-keeping and stressful budgeting<br />
-                        Let WalletWhiz do the heavy lifting for you. WalletWhiz makes it quick and easy to keep an eye on
-                        your finances, so you can spend more time on what matters
+                        WalletWhiz helps you track and categorize your spending<br />
+                        so you can know exactly what you're spending your money on<br />
+                        WalletWhiz gives you the tools to manage your money with ease <br />
+                        so you can spend time on what really matters. 
                     </p>
+                </div>
                 </div>
             </div>
             <div className='about2'>
-
+                <div className='framer'>
+            <div className='pageTextBox'>
+                    <h2 className="headerText blackText">Plan for the future.</h2>
+                    <p className="descriptionSubText blackText">
+                        We get that your future is important to you, Thats why WalletWhiz<br />
+                        Your time is precious to you, why waste it on hours of book-keeping and stressful budgeting<br />
+                        WalletWhiz gives you the tools to manage your money with ease <br />
+                        so you can spend time on what really matters. 
+                    </p>
+                </div>
+                </div>
+            </div>
+            <div className='about1'>
+            <div className='pageTextBox'>
+                    <h2 className="headerText blackText">Plan for the future.</h2>
+                    <p className="descriptionSubText blackText">
+                        We get that your future is important to you, Thats why WalletWhiz<br />
+                        Your time is precious to you, why waste it on hours of book-keeping and stressful budgeting<br />
+                        WalletWhiz gives you the tools to manage your money with ease <br />
+                        so you can spend time on what really matters. 
+                    </p>
+                </div>
             </div>
         </div>
     );
