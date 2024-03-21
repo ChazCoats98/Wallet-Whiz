f18bb6a20a2f4d860d150771a13cfa83766b5ad7
Chaz Coats <chazcoats2@hotmail.com>
2024-03-21 09:53:54 -0400

    Working on front end stock search functionality and adding saving stocks to the database

--- client/src/pages/stock-pages/portfolio.jsx
+++ client/src/pages/stock-pages/portfolio.jsx
@@ -1,12 +1,24 @@
 import ResponsiveAppBar from "../../components/nav";
+import { useState } from "react";
 import { useQuery } from "@apollo/client";
+import { IconButton, Paper, InputBase, Divider } from "@mui/material";
+import SearchIcon from '@mui/icons-material/Search';
 import { FETCH_STOCK_BY_TICKER } from "../../utils/queries";
 import ComponentLoader from "../../components/ComponentLoader";
 
 const Portfolio = () => {
-    const {loading, error, data} = useQuery(FETCH_STOCK_BY_TICKER, {variables: {input: 'AAPL'}});
+    const {userInput, setUserInput} = useState('AAPL');
+    const {loading, error, data} = useQuery(FETCH_STOCK_BY_TICKER, {variables: {input: "AAPL"}});
     if (loading) return <ComponentLoader />
-    if (error) return <p>Error: {error.message}</p> 
+    if (error) return <p>Error: {error.message}</p>
+    
+    const getUserValue = (e) => {
+        const value = e.target.value
+        setUserInput(value)
+    }
+
+    const stocks = [];
+    console.log(userInput);
 
     console.log(data)
     return (
@@ -14,7 +26,24 @@ const Portfolio = () => {
             <div className='nav-no-animation'>
                 <ResponsiveAppBar />
             </div>
-            <h1>Portfolio Page</h1>
+            <div className='main-container'>
+                <h1>Portfolio Page</h1>
+                <div className='search-container'>
+                    <Paper
+                    component="form"
+                    sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 400 }}>
+                        <InputBase
+                            sx={{ ml: 1, flex: 1 }}
+                            placeholder="Search Google Maps"
+                            inputProps={{ 'aria-label': 'search by ticker' }}
+                        />
+                        <Divider />
+                        <IconButton type='submit' aria-label='search'>
+                            <SearchIcon />
+                        </IconButton>
+                    </Paper>
+                </div>
+            </div>
         </div>
     )
 }
---
+++ server/models/Stocks.js
@@ -0,0 +1,36 @@
+const { Schema } = require('mongoose');
+
+const stockSchema = new Schema({
+    _id: {
+        type: String,
+    },
+    symbol: {
+        type: String,
+    },
+    price: {
+        type: Number,
+    },
+    mktCap: {
+        type: Number,
+    },
+    changes: {
+        type: Number,
+    },
+    companyName: {
+        type: String,
+    },
+    exchange: {
+        type: String,
+    },
+    industry: {
+        type: String,
+    },
+    sector: {
+        type: String,
+    },
+    image: {
+        type: String,
+    }
+});
+
+module.exports = stockSchema;
